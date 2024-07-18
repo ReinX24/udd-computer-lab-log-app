f52c36436b71d2d255e5ea9750e9a3a861fbbd20
Rein <reinX244@gmail.com>
2024-07-18 14:56:19 +0800

    Started removing feedback_app files

--- public/index.php
+++ public/index.php
@@ -4,11 +4,10 @@
 require_once __DIR__ . "/../vendor/autoload.php";
 
 use app\Router;
-use app\controllers\FeedbackController;
 use app\controllers\AdminController;
 use app\controllers\LogController;
 
-// TODO: segregate Logs and Feedback routes
+// LOG VERSION
 // TODO: make different databases 
 
 $router = new Router();
@@ -38,47 +37,34 @@ $router->addPostRoute("/log/admin_login", [AdminController::class, "admin_login"
 // END OF LOG ROUTES
 
 // START OF FEEDBACK ROUTES
-$router->addGetRoute("/feedback/create", [FeedbackController::class, "feedback_create"]);
-$router->addPostRoute("/feedback/create", [FeedbackController::class, "feedback_create"]);
+// $router->addGetRoute("/feedback/create", [FeedbackController::class, "feedback_create"]);
+// $router->addPostRoute("/feedback/create", [FeedbackController::class, "feedback_create"]);
 
-$router->addGetRoute("/feedback/admin_login", [AdminController::class, "admin_login"]);
-$router->addPostRoute("/feedback/admin_login", [AdminController::class, "admin_login"]);
+// $router->addGetRoute("/feedback/admin_login", [AdminController::class, "admin_login"]);
+// $router->addPostRoute("/feedback/admin_login", [AdminController::class, "admin_login"]);
 // END OF FEEDBACK ROUTES
 
 // START OF ADMIN ROUTES
 $router->addGetRoute("/admin/dashboard", [AdminController::class, "admin_dashboard"]);
 
+// START OF ADMIN ROUTES FOR LOGS
+//* Add student_id for admin
 $router->addGetRoute("/admin/search_log", [AdminController::class, "admin_search_log"]);
 
-// START OF ADMIN ROUTES FOR LOGS
-//* DONE: add student_id for admin
 $router->addGetRoute("/admin/search_log/add_student_id", [AdminController::class, "add_student_id"]);
 $router->addPostRoute("/admin/search_log/add_student_id", [AdminController::class, "add_student_id"]);
 
-//* DONE: add time_out for admin here
+//* Add time_out for admin
 $router->addGetRoute("/admin/search_log/add_time_out", [AdminController::class, "add_time_out"]);
 $router->addPostRoute("/admin/search_log/add_time_out", [AdminController::class, "add_time_out"]);
 
-//* DONE: add edit for admin
+//* Edit log for admin
 $router->addGetRoute("/admin/search_log/log_edit", [AdminController::class, "log_edit"]);
 $router->addPostRoute("/admin/search_log/log_edit", [AdminController::class, "log_edit"]);
 
-//* DONE: add delete for admin
+//* Delete log for admin
 $router->addPostRoute("/admin/search_log/log_delete", [AdminController::class, "log_delete"]);
 // END OF ADMIN ROUTES FOR LOGS
-
-// START OF ADMIN ROUTES FOR FEEDBACK
-$router->addGetRoute("/admin/search", [AdminController::class, "admin_search"]);
-$router->addGetRoute("/admin/search/details", [AdminController::class, "admin_search_details"]);
-
-// Only master accounts can edit searches
-$router->addGetRoute("/admin/search/edit", [AdminController::class, "admin_search_edit"]);
-$router->addPostRoute("/admin/search/edit", [AdminController::class, "admin_search_edit"]);
-
-// Only master accounts can delete searches
-$router->addGetRoute("/admin/search/delete", [AdminController::class, "admin_search_delete"]);
-$router->addPostRoute("/admin/search/delete", [AdminController::class, "admin_search_delete"]);
-// END OF ADMIN ROUTES FOR FEEDBACK
 
 // START OF ADMIN ROUTES FOR ACCOUNTS
 // View all existing accounts
